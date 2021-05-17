f65bfc10c2a6dc1ea96e43580010088fca4f06b4
klarabau <klarabaumeister@gmail.com>
2021-05-17 18:39:07 +0200

    04 Animation FINAL

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,28 +1,15 @@
 
-
 // changing individual properties with code and using setInterval
 var rotationSpeed = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
+	rotationSpeed = 0.05;
 	myOtherBox.object3D.rotation.x -= rotationSpeed;
+	rotationSpeed = 0.01;
+	myOtherBox.object3D.rotation.y -= rotationSpeed;
+	rotationSpeed = 0.005;
+	myOtherBox.object3D.rotation.z -= rotationSpeed;
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
-
-
-
-// changing individual properties with code and using setInterval
-// var rotationSpeed = 0.01;
-// var myOtherBox = document.getElementById('myOtherBox');
-
-// function spin(){
-// 	rotationSpeed = 0.05;
-// 	myOtherBox.object3D.rotation.x -= rotationSpeed;
-// 	rotationSpeed = 0.01;
-// 	myOtherBox.object3D.rotation.y -= rotationSpeed;
-// 	rotationSpeed = 0.005;
-// 	myOtherBox.object3D.rotation.z -= rotationSpeed;
-// }
-
-// setInterval(spin, 16); //equivalent to 60 fps
