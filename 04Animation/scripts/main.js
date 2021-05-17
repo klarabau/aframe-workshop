e7a01bb62e42bd29e6a5ad67faac801f43332fdc
klarabau <klarabaumeister@gmail.com>
2021-05-17 17:39:52 +0200

    04 Animation Other Axes

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -7,6 +7,10 @@ var myOtherBox = document.getElementById('myOtherBox');
 function spin(){
 	myOtherBox.object3D.rotation.x -= rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.x);
+
+	myOtherBox.object3D.rotation.y -= rotationSpeed;
+
+	myOtherBox.object3D.rotation.z -= rotationSpeed;
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
