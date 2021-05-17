29374fad04ba8b8110a8e945f0167a1332f4c475
klarabau <klarabaumeister@gmail.com>
2021-05-17 17:57:42 +0200

    04 Animation Different Speeds

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,7 +1,7 @@
 
 
 // changing individual properties with code and using setInterval
-var rotationSpeed = 0.05;
+var rotationSpeed = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
@@ -10,6 +10,19 @@ function spin(){
 	myOtherBox.object3D.rotation.y -= rotationSpeed;
 
 	myOtherBox.object3D.rotation.z -= rotationSpeed;
+
+
+	if (rotationSpeed <= 0.01){
+		while (rotationSpeed < 0.09){
+			rotationSpeed += 0.01;
+		}
+	}
+
+	if (rotationSpeed >= 0.09){
+		while (rotationSpeed > 0.01){
+			rotationSpeed -= 0.01;
+		}
+	} 
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
